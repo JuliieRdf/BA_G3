5d203a803b8b4a5da07346675ee3b8fb1eb44420
JuliieRdf <julie.rondof@mmibordeaux.com>
2023-02-07 14:54:27 +0100

    Update index.php
    Test

--- index.php
+++ index.php
@@ -21,8 +21,7 @@ $lastP3 = sql_select("Article","*",null, "dtCreArt DESC", 3)[2]['urlPhotArt'];
 <!DOCTYPE html>
 <html>
 
-
-
+<h1></h1>
 <body>
     <div class="slideshow">
         <div class="slideshow-container">
