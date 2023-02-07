075ea84aa2f1419abdfc7efdbf7ab1dbb1baeb5f
Ninodev14 <nino.lefort@mmibordeaux.com>
2023-02-07 18:13:02 +0100

    pas finis form

--- views/backend/articles/create.php
+++ views/backend/articles/create.php
@@ -1,5 +1,5 @@
 <head>
-<link rel="stylesheet" href="../../../source/css/crearticle.css">
+    <link rel="stylesheet" href="../../../source/css/crearticle.css">
 </head>
 <?php
 include '../../../header.php';
@@ -7,72 +7,88 @@ include '../../../header.php';
 
 <body>
     <div class="row">
-    
         <div class="col-3">
-            <div class="deco"><div class="cercle-g"></div></div>
+            <div class="deco-verti">
+                <div class="cercle-g"></div>
+            </div>
         </div>
         <div class="col-6">
             <h1 class="titre">Ajouter un nouvel article</h2>
         </div>
         <div class="col-3">
-        
-            <div class="deco"><div class="cercle-d"></div></div>
+            <div class="deco-verti">
+                <div class="cercle-d"></div>
+            </div>
         </div>
     </div>
 
-<!--Bootstrap form to create a new status-->
+    <!--Bootstrap form to create a new status-->
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Entrez votre nouvel article</h1>
+
+        <div class="row">
+            <div class="col-1">
+                <div class="deco-hori"></div>
+            </div>
+            <div class="col-md-3">
+                <label for="libTitrArt">
+                    <h1>Titre</h1>
+                </label>
+                <label for="libChapoArt">
+                    <h1>Chapeau</h1>
+                </label>
+                <label for="libAccrochArt">
+                    <h1>Accroche</h1>
+                </label>
+                <label for="parag1Art">
+                    <h1>Paragraphe 1</h1>
+                </label>
+                <label for="libSsTitr1Art">
+                    <h1>Sous Titre 1</h1>
+                </label>
+                <label for="parag2Art">
+                    <h1>Paragraphe 2</h1>
+                </label>
+                <label for="libSsTitr2Art">
+                    <h1>Sous Titre 2</h1>
+                </label>
+                <label for="parag3Art">
+                    <h1>Paragraphe 3</h1>
+                </label>
+                <label for="libConclArt">
+                    <h1>Conclusion</h1>
+                </label>
+                <label for="urlPhotArt">
+                    <h1>Photo</h1>
+                </label>
+                <label for="numThem">
+                    <h1>Numéro Thème</h1>
+                </label>
+            </div>
+            <div class="col-7">
+                <!--Form to create a new status-->
+                <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
+                    <div class="form-group">
+                        <input id="libTitrArt" class="form-control" placeholder="Recherche..." type="text" name="libTitrArt">
+                        <input id="libChapoArt" class="form-control" placeholder="Recherche..." type="text" name="libChapoArt">
+                        <input id="libAccrochArt" class="form-control" placeholder="Recherche..." type="text" name="libAccrochArt">
+                        <input id="parag1Art" class="form-control" placeholder="Recherche..." type="text" name="parag1Art">
+                        <input id="libSsTitr1Art" class="form-control" placeholder="Recherche..." type="text" name="libSsTitr1Art">
+                        <input id="parag2Art" class="form-control" placeholder="Recherche..." type="text" name="parag2Art">
+                        <input id="libSsTitr2Art" class="form-control" placeholder="Recherche..." type="text" name="libSsTitr2Art">
+                        <input id="parag3Art" class="form-control" placeholder="Recherche..." type="text" name="parag3Art">
+                        <input id="libConclArt" class="form-control" placeholder="Recherche..." type="text" name="libConclArt">
+                        <input id="urlPhotArt" class="form-control" placeholder="Recherche..." type="text" name="urlPhotArt">
+                        <input id="numThem" class="form-control" placeholder="Recherche..." type="text" name="numThem">
+                    </div>
+                    <div class="form-group mt-2">
+                        <button type="submit" class="btn btn-success">Créer</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="col-md-12">
-            <!--Form to create a new status-->
-            <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
-                <div class="form-group">
-                    <label for="libTitrArt">Titre</label>
-                    <input id="libTitrArt" class="form-control" type="text" name="libTitrArt">
+    <div class="col-1"></div>
 
-                    <label for="libChapoArt">Chapeau</label>
-                    <input id="libChapoArt" class="form-control" type="text" name="libChapoArt">
-
-                    <label for="libAccrochArt">Accroche</label>
-                    <input id="libAccrochArt" class="form-control" type="text" name="libAccrochArt">
-
-                    <label for="parag1Art">Paragraphe 1</label>
-                    <input id="parag1Art" class="form-control" type="text" name="parag1Art">
-
-                    <label for="libSsTitr1Art">Sous Titre 1</label>
-                    <input id="libSsTitr1Art" class="form-control" type="text" name="libSsTitr1Art">
-
-                    <label for="parag2Art">Paragraphe 2</label>
-                    <input id="parag2Art" class="form-control" type="text" name="parag2Art">
-
-                    <label for="libSsTitr2Art">Sous Titre 2</label>
-                    <input id="libSsTitr2Art" class="form-control" type="text" name="libSsTitr2Art">
-
-                    <label for="parag3Art">Paragraphe 3</label>
-                    <input id="parag3Art" class="form-control" type="text" name="parag3Art">
-
-                    <label for="libConclArt">Conclusion</label>
-                    <input id="libConclArt" class="form-control" type="text" name="libConclArt">
-                    
-                    <label for="urlPhotArt">Photo</label>
-                    <input id="urlPhotArt" class="form-control" type="text" name="urlPhotArt">
-
-                    <label for="numThem">Numéro Thème</label>
-                    <input id="numThem" class="form-control" type="text" name="numThem">
-
-
-                </div>
-                <div class="form-group mt-2">
-                    <button type="submit" class="btn btn-success">Créer</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
+    <?php include '../../../footer.php'; ?>
 </body>
+
 </html>
