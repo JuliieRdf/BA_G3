f12d7290fecd9a489ea7cec05608e2ab82790e9e
Ninodev14 <nino.lefort@mmibordeaux.com>
2023-02-07 16:37:25 +0100

    non enfaite
    re répartion du header

--- header.php
+++ header.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Art</title>
+    <!-- Load CSS -->
+    <link rel="stylesheet" href="/source/css/slide.css">
+    <link rel="stylesheet" href="/source/css/header.css">
+    <link rel="stylesheet" href="/source/css/footer.css">
+    <!-- Bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="/source/css/dashboard.css">
+</head>
 
 <?php
 //load config
