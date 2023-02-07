28838dbb3d7b75e08df28e7cc3b4725d6767f479
TheSloum <maelvirgil@gmail.com>
2023-02-07 17:27:32 +0100

    rectification des boutons

--- index.php
+++ index.php
@@ -213,35 +213,11 @@ transform: translate(0px,-50px);">
         
             </div>
             </div>
-            <footer>
-    <ul class="nav justify-content-center" style="background-color: #6D071A; ">
-        <div class="col-3">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"><img src="/source/images/Logo_blanc.png" alt="Logo Bordeaux d'éraille blanc" width="230px" height="auto"></a>
-            </li>
-        </div>
-        <div class="col-3">
-            <li class="nav-item">
-                <a class="nav-link" href="#" style="color: #ffffff; text-decoration:underline;">S'inscire</a>
-            </li>
-        </div>
-        <div class="col-3">
-            <li class="nav-item">
-                <a class="nav-link" href="#" style="color: #ffffff; text-decoration:underline;">Se connecter</a>
-            </li>
-        </div>
-        <div class="col-3">
-            <li class="nav-item">
-                <a class="nav-link" href="crearticle.php" style="color: #ffffff; text-decoration:underline;">Mention Légal</a>
-            </li>
-        </div>
-    </ul>
 
-</footer>
+
+    <?php require_once 'footer.php'; ?>
+    
     </div>
-
-
-
 
 
 
