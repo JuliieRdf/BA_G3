2db83b6c006c503df1252cf1266f1eb5d494f454
Clairedelunee <115539104+Clairedelunee@users.noreply.github.com>
2023-02-07 16:36:09 +0100

    correcteur footer

--- footer.php
+++ footer.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Art</title>
-    <!-- Load CSS -->
-    <link rel="stylesheet" href="/source/css/footer.css">
-    <!-- Bootstrap CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
 
 <footer>
     <ul class="nav justify-content-center" style="background-color: #6D071A; ">
