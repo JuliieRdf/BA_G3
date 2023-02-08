2863d3773e4038645cad95b953c3eab044737e37
TheSloum <maelvirgil@gmail.com>
2023-02-08 16:45:17 +0100

    e

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
         </div>
         <div class="col-3">
             <li class="nav-item">
-                <a class="nav-link" href="views/frontend/articles/article.php" style="padding-top: 3vh; color: #ffffff; text-decoration:underline;"><h3 class="typo-footer">Mention Légal</h3></a>
+                <a class="nav-link" href="views/frontend/articles/article.php" style="padding-top: 3vh; color: #ffffff; text-decoration:underline;"><h3 class="typo-footer">Mentions Légales</h3></a>
             </li>
         </div>
     </ul>
