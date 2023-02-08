657d9685d8e3aaaf3c9d07ba8628baa4906347d3
JuliieRdf <julie.rondof@mmibordeaux.com>
2023-02-08 18:16:25 +0100

    Correction Se Connecter

--- api/security/login.php
+++ api/security/login.php
@@ -12,15 +12,6 @@ $passMembPost = sql_escape($_POST['passMemb']);
 $password = sql_select("MEMBRE","passMemb","pseudoMemb = '$pseudoMembPost'")[0]['passMemb'];
 
 if (password_verify($passMembPost, $password)) {
-    echo 'Password is valid!';
-} else {
-    echo 'Invalid password.';
-}
-
-
-
-
-if ($passCheck == $pseudoCheck){  
     $_SESSION["pseudo"] = $pseudoMemb;
     header('Location: ../../views/frontend/compte.php');
 } else {
