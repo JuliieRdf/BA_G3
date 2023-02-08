f7e324d0faae9943aa504adfcd2c892bb0df2b12
TheSloum <maelvirgil@gmail.com>
2023-02-08 09:15:31 +0100

    rectification

--- index.php
+++ index.php
@@ -18,22 +18,6 @@ $lastP3 = sql_select("Article","*",null, "dtCreArt DESC", 3)[2]['urlPhotArt'];
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Load CSS -->
-    <link rel="stylesheet" href="source/css/index.css">
-
-    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-    <!-- Bootstrap CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
 <html>
 
 
