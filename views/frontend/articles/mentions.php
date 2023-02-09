6e310c32d2f08606a87d8040721f18e5861a7b0e
TheSloum <maelvirgil@gmail.com>
2023-02-09 16:44:12 +0100

    mentions légales + rgpd

---
+++ views/frontend/articles/mentions.php
@@ -0,0 +1,40 @@
+<?php require_once '../../../header.php'; ?>
+
+<body>
+    <div class="gutter container-fluid">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1"><h1><br>Mentions Légales</h1></div>
+            
+            <div class="col-10"></div>
+            
+            <div class="col-2"></div>
+            <div class="col-6">
+
+
+            <h2><br><br><br>Propriétaire</h2>
+                <p>Pixel Perfect<br>
+1 Rue Jacques Ellul, 33800 Bordeaux<br>
+07 62 20 09 14<br>
+pixelperfect@gmail.com<br><br><br>
+</p>
+                <h2>Fournisseur d'hébèrgement</h2>
+                <p>Nom<br>
+Localisation<br>
+Tél<br><br><br>
+</p>
+                <h2>Avertissement informationel</h2>
+                <p>La consultation des articles de Bordeaux Déraille est gratuite et sans aucune garantie de la part de l’éditeur.
+Les informations qui proviennent de sources extérieures ne sont pas garanties d’être sans erreurs.
+Il en vient donc à la responsabilité  de l’utilisateur d’utiliser ses informations à ses risques et de les vérifier, et de son interprétation et des conséquences qui en découlent.<br><br><br></p>
+<a href="rgpd.php"><h2>RGPD</h2></a>
+
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    <?php include '../contact.php'; ?>
+</body>
+
+<?php include '../../../footer.php'; ?>
---
+++ views/frontend/articles/rgpd.php
@@ -0,0 +1,41 @@
+<?php require_once '../../../header.php'; ?>
+
+<body>
+    <div class="gutter container-fluid">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1"><h1><br>RGPD</h1></div>
+            
+            <div class="col-10"></div>
+            
+            <div class="col-2"></div>
+            <div class="col-6">
+
+                <p><br><br><br>Le texte suivant portera sur l’information sur la collecte de données à caractère personnel.<br><br>
+
+                    Vos cookies ne seront pas utilisés à fins publicitaires, la collecte des cookies est strictement nécessaire à votre navigation.<br><br>
+
+Les informations recueillies pour la connexion (Pseudonyme, Mot de passe) sont enregistrées dans un fichier informatisé par Pixel Perfect [07 62 20 09 14] pour la création de compte. La base légale du traitement est le consentement.<br><br>
+
+Toutes données d’information concernant votre vie privée, publique ou professionnelle, ne seront pas communiquées.<br><br>
+
+Les données sont conservées pendant 30 jours.<br><br>
+
+Vous pouvez accéder aux données vous concernant, les rectifier, demander leur effacement ou exercer votre droit à la limitation du traitement de vos données. Vous pouvez retirer à tout moment votre consentement au traitement de vos données ; Vous pouvez également vous opposer au traitement de vos données ; Vous pouvez également exercer votre droit à la portabilité de vos données.<br><br>
+
+Consultez le site cnil.fr pour plus d’informations sur vos droits.<br><br>
+
+Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, vous pouvez contacter en cas échéant, notre délégué à la protection des données ou le service chargé de l’exercice de ces droits :Pixel Perfect [07 62 20 09 14] <br><br>
+
+Si vous estimez, après nous avoir contactés, que vos droits « Informatique et Libertés » ne sont pas respectés, vous pouvez adresser une réclamation à la CNIL sur leur site, par le téléservice de plainte en ligne, ou par courrier postal en écrivant à CNIL - Service des Plaintes - 3 Place de Fontenoy - TSA 80715 - 75334 PARIS CEDEX 07.<br><br>
+</p>
+            </div>
+            
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    <?php include '../contact.php'; ?>
+</body>
+
+<?php include '../../../footer.php'; ?>
