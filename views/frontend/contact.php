853b7d0a5571c84c05557db00e8a048db0fb56ac
Clairedelunee <115539104+Clairedelunee@users.noreply.github.com>
2023-02-09 15:40:04 +0100

    responsive page article

--- views/frontend/contact.php
+++ views/frontend/contact.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contact</title>
-</head>
 
 <body>
     <?php
     if (isset($_POST["message"])) {
-$message = "Ce message vous a été envoyé via la page du site BordeauxDéraile
+        $message = "Ce message vous a été envoyé via la page du site BordeauxDéraile
 Nom :" . $_POST["email"] . "
 Message : " . $_POST["message"];
         $retour = mail("claire.vital@mmibordeaux.com", $_POST["sujet"], $message, "From:clairevital00@gmail.com\r\nReply-to:" . $_POST["email"]);
@@ -29,9 +21,9 @@ Message : " . $_POST["message"];
                 <label for="email">E-Mail:</label><br>
                 <input type="email" name="email" required><br>
                 <label for="message">Message:</label><br>
-                <textarea name="message" required cols="30" rows="10"></textarea>
+                <textarea name="message" required ></textarea>
             </div>
-            <input type="submit" value="Envoyer" class="poster">
+            <input type="submit" value="Envoyer"class="poster">
         </form>
 
 
