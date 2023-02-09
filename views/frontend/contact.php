4b898a3a16188e72aaf807e26428a9ee4e7ce6ba
Clairedelunee <115539104+Clairedelunee@users.noreply.github.com>
2023-02-09 13:08:00 +0100

    formulaire de contact fonctionnel

--- views/frontend/contact.php
+++ views/frontend/contact.php
@@ -1,20 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST["message"])) {
+$message = "Ce message vous a été envoyé via la page du site BordeauxDéraile
+Nom :" . $_POST["email"] . "
+Message : " . $_POST["message"];
+        $retour = mail("claire.vital@mmibordeaux.com", $_POST["sujet"], $message, "From:clairevital00@gmail.com\r\nReply-to:" . $_POST["email"]);
+        if ($retour) {
+            echo "<p> l'email à bien été envoyé </p>";
+        }
+    }
+    ?>
+    <section class="contact">
+        <h3 class="contact">CONTACTEZ NOUS</h3>
+        <form class="contacter" method="post">
+            <div class="contacter_b">
+                <label for="Sujet" class="contact-top">Sujet:</label><br>
+                <input type="text" name="sujet" required><br>
+                <label for="email">E-Mail:</label><br>
+                <input type="email" name="email" required><br>
+                <label for="message">Message:</label><br>
+                <textarea name="message" required cols="30" rows="10"></textarea>
+            </div>
+            <input type="submit" value="Envoyer" class="poster">
+        </form>
 
 
-<section class="contact">
-                    <h3 class="contact">CONTACTEZ NOUS</h3>
-                    <form class="contacter">
-                        <div class="contacter_b">
-                            <label for="nom" class="contact-top">Nom:</label><br>
-                            <input type="text" id="text" name="text"><br>
-                            <label for="prenom">Prénom:</label><br>
-                            <input type="text" id="prenom" name="prenom"><br>
-                            <label for="email">E-Mail:</label><br>
-                            <input type="text" id="email" name="email"><br>
-                            <label for="message">Message:</label><br>
-                            <textarea name="message" id="message" cols="30" rows="10"></textarea>
-                        </div>
-                        <input type="submit" value="Poster" class="poster">
-                    </form>
+    </section>
+</body>
 
-
-                </section>
+</html>
