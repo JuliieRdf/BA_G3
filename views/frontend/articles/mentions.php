8410cfa2a2bfaf180b6ce3080f4f142f56f8696f
Ninodev14 <nino.lefort@mmibordeaux.com>
2023-02-09 16:59:01 +0100

    mise a jour des rgpd

--- views/frontend/articles/mentions.php
+++ views/frontend/articles/mentions.php
@@ -18,10 +18,10 @@
 07 62 20 09 14<br>
 pixelperfect@gmail.com<br><br><br>
 </p>
-                <h2>Fournisseur d'hébèrgement</h2>
-                <p>Nom<br>
-Localisation<br>
-Tél<br><br><br>
+                <h2>Hébergeur</h2>
+                <p>IUT Bordeaux Montaigne<br>
+                1 rue Jacques Ellul 33080 Bordeaux Cedex France<br>
++33 5 57 12 20 44<br><br><br>
 </p>
                 <h2>Avertissement informationel</h2>
                 <p>La consultation des articles de Bordeaux Déraille est gratuite et sans aucune garantie de la part de l’éditeur.
@@ -37,4 +37,25 @@ Il en vient donc à la responsabilité  de l’utilisateur d’utiliser ses info
     <?php include '../contact.php'; ?>
 </body>
 
-<?php include '../../../footer.php'; ?>
+<footer>
+    <ul class="nav justify-content-center" style="padding: 1vh; background-color: #6D071A;">
+        <div class="col-4">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><img src="/source/images/Logo_blanc.png" alt="Logo Bordeaux déraille blanc" width="230px" height="auto"></a>
+            </li>
+        </div>
+        <div class="col-4">
+            <li class="nav-item">
+                <a class="nav-link" href="views/backend/security/signup.php" style="padding-top: 3vh; color: #ffffff; text-decoration:underline;"><h3 class="typo-footer">S'inscrire</h3></a>
+            </li>
+        </div>
+        <div class="col-4">
+            <li class="nav-item">
+                <a class="nav-link" href="views/backend/security/login.php" style="padding-top: 3vh; color: #ffffff; text-decoration:underline;"><h3 class="typo-footer">Se connecter</h3></a>
+            </li>
+        </div>
+
+    </ul>
+</footer>
+
+</html>
